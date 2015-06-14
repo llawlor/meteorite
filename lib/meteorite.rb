47d947ce8f47675b42337e75c3ed7511c865a467
llawlor <primemod3@gmail.com>
2015-06-14 17:42:11 -0400

    add bind_key for instances

--- lib/meteorite.rb
+++ lib/meteorite.rb
@@ -1,8 +1,14 @@
 require "meteorite/version"
 
 module Meteorite
+  # mount the Rails engine so that our JavaScript assets are included
   module Rails
     class Engine < ::Rails::Engine
     end
+  end
+  
+  # get the unique bind key for an instance of an ActiveRecord model
+  def self.bind_key(instance)
+    "#{instance.class.name}-#{instance.id}"
   end
 end
