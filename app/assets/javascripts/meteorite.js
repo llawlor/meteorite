afd9a296db1468c2d1f643d8aad118f9ec05f72e
llawlor <primemod3@gmail.com>
2015-06-14 21:45:13 -0400

    working on checkbox state

--- app/assets/javascripts/meteorite.js
+++ app/assets/javascripts/meteorite.js
@@ -14,15 +14,20 @@ ws.onmessage = function(msg) {
   // for each meteorite class
   $('.meteorite').each(function() {
     // if the bind keys match
-    if ($(this).data('bind_key') === json.bind_key) {
+    if ($(this).data('bind-key') === json.bind_key) {
+      console.log('bind found');
       // get the bind data
       var bind_data = JSON.parse(json.bind_data);
       // desired attribute
-      var bind_attr = $(this).data('bind_attr');
+      var bind_attr = $(this).data('bind-attr');
       // get the attribute data
       var attr_data = JSON.stringify(bind_data[bind_attr]);
-      // update the attribute
+      // update the text attribute
       $(this).text(attr_data);
+      console.log(bind_attr);
+      console.log(attr_data);
+      // update the property
+      $(this).prop(bind_attr, attr_data);
     }
   });
 }
