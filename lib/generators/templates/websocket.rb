d2865ae044e3a349740527783168bfe7d3e694b5
llawlor <primemod3@gmail.com>
2015-06-17 22:38:30 -0400

    keep track of connected clients

--- lib/generators/templates/websocket.rb
+++ lib/generators/templates/websocket.rb
@@ -2,16 +2,30 @@ require 'em-websocket'
 require 'em-hiredis'
 require 'json'
 
+$clients = []
+
 EM.run do
   puts "starting websocket server. . ."
   
   redis = EM::Hiredis.connect #("redis://127.0.0.1:6379")
   pubsub = redis.pubsub
 
+  # when a pubsub message is received
+  pubsub.on(:message) do |channel, message|
+    puts [:message, channel, message]
+    # for each client
+    $clients.each do |client|
+      # send a message
+      client.send({bind_key: channel, bind_data: message}.to_json)
+    end
+  end
+    
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
     ws.onopen do |handshake|
       puts "WebSocket connection open"
-
+      
+      # add the client
+      $clients.push(ws)
       # Access properties on the EM::WebSocket::Handshake object, e.g.
       # path, query_string, origin, headers
 
@@ -19,7 +33,10 @@ EM.run do
       #ws.send "Hello Client, you connected to #{handshake.path}"
     end
 
-    ws.onclose { puts "Connection closed" }
+    ws.onclose do
+      puts "Connection closed"
+      $clients.delete(ws)
+    end
 
     ws.onmessage do |msg|
       puts "Received message: #{msg}"
@@ -32,12 +49,6 @@ EM.run do
         pubsub.subscribe(json['key'])
         puts "subscribe to: #{json['key']}"
       end
-    end
-    
-    # when a pubsub message is received
-    pubsub.on(:message) do |channel, message|
-      puts [:message, channel, message]
-      ws.send({bind_key: channel, bind_data: message}.to_json)
     end
   
   end
